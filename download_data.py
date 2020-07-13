8362b252a9822912b369d9ba0939c377835c1284
Michael Osthege <m.osthege@fz-juelich.de>
2020-07-13 19:26:03 +0200

    Also download RKI nowcasting
    The RKI publishes a nowcasting of R that is updated on a daily basis (at least that's what they write).
    Unfortunately they don't offer daily download links.

--- download_data.py
+++ download_data.py
@@ -2,3 +2,8 @@ from arcgis.gis import GIS
 anon_gis = GIS()
 features = anon_gis.content.get('dd4580c810204019a7b8eb3e0b329dd6').tables[0].query()
 features.save(save_location='.', out_name="data.csv")
+
+import requests
+url = 'https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Projekte_RKI/Nowcasting_Zahlen.xlsx?__blob=publicationFile'
+with open('nowcasting.xlsx', 'wb') as file:
+    file.write(requests.get(url).content)
